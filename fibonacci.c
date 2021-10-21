f30afb68edd65c159e5bf66331e2cf4139e0cfff
Adam Brailove <adam.brailove@gmail.com>
2021-10-21 16:00:08 -0700

    first commit

---
+++ fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int fibmax = 30;
+
+    int fibprevprev = 0;
+    int fibprev = 1;
+    int i = 1;
+    printf("%d: %d\n", i, fibprev);
+
+    for ( i=2; i < fibmax; i++) {
+        int fibnext = fibprev + fibprevprev;
+        printf("%d: %d\n", i, fibnext);
+        fibprevprev = fibprev;
+        fibprev = fibnext;
+    }
+}
